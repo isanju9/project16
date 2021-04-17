f037c8cc46d3c861a35bd62654ab95f4a1657908
isanju9 <82690677+isanju9@users.noreply.github.com>
2021-04-17 14:23:49 +0530

    Add files via upload

---
+++ project16/website1/views.py
@@ -0,0 +1,19 @@
+from website1.forms import MovieForm
+from website1.models import Movie
+from django.shortcuts import render
+
+def index_view(request):
+	return render(request,'website1/index.html')
+
+
+def add_movie_view(request):
+	form=MovieForm()
+	if request.method=='POST':
+		form=MovieForm(request.POST)
+		if form.is_valid():
+			form.save(commit=True)
+	return render(request,'website1/addmovie.html',{'form':form})
+
+def list_movie_view(request):
+	movies_list=Movie.objects.all().order_by('-rating')
+	return render(request,'website1/listmovie.html',{'movies_list':movies_list})
